622b32173895f39747bf4b4b2e1a51f97037761e
Kiara Jimena Gonzalez Almanzar <gonz1135@msu.edu>
2025-06-26 01:49:02 -0400

    Initial setup: Vite + React + JAFA title page

---
+++ jafa/src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css"; // assuming you're using App.css
+
+function App() {
+  return (
+    <div className="container">
+      <h1 style={{fontFamily: "Georgia"}}>JAFA Game</h1>
+    </div>
+  );
+}
+
+export default App;
